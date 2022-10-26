baf4802d05063799a57b8e223cc5ee7ce5c68ca2
Timur Pavlov <pavlov.timur556@yandex.ru>
2022-10-26 08:13:32 +0300

    feat (tests): added more tests for handlers

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -4,6 +4,7 @@ from aiogram.filters import StateFilter
 from .middleware import TestMiddleware
 from aiogram_tests.handler import MessageHandler
 from aiogram_tests.handler import RequestHandler
+from aiogram_tests.handler import TelegramEventObserverHandler
 
 
 def test_request_handler_initialization():
@@ -18,6 +19,18 @@ def test_request_handler_dp_middlewares():
     r_h = RequestHandler(dp_middlewares=(TestMiddleware(), TestMiddleware()))
     middlewares_count = len(r_h.dp.message.middleware)
     assert middlewares_count == 2
+
+    r_h = RequestHandler(dp_middlewares=(TestMiddleware(), TestMiddleware()), exclude_observer_methods=["message"])
+    middlewares_count = len(r_h.dp.message.middleware)
+    assert middlewares_count == 0
+
+
+def test_telegram_observ_methods_handler_init():
+    async def callback(*args, **kwargs):
+        pass
+
+    with pytest.raises(ValueError):
+        _ = TelegramEventObserverHandler(callback, state_data=[])
 
 
 @pytest.mark.asyncio
@@ -34,3 +47,19 @@ async def test_telegram_observ_handler():
     await t_h(None)
     handlers_count = len(t_h.dp.message.handlers)
     assert handlers_count == 1
+
+
+@pytest.mark.asyncio
+async def test_state_telegram_observ_handler():
+    async def callback(*args, **kwargs):
+        pass
+
+    t_h = MessageHandler(callback, state="state", state_data={"name": "Mike"})
+    await t_h(None)
+
+    context = t_h.dp.fsm.get_context(t_h.bot, 12345678, 12345678)
+    state = await context.get_state()
+    data = await context.get_data()
+
+    assert state == "state"
+    assert data == {"name": "Mike"}
